4e437b67879d74c8539407e95fb0411d3d3ab689
John Fan <johnfan888@hotmail.com>
2018-05-10 11:31:25 +0800

    adjust directory

--- src/1.0/managerserver/key.php
+++ src/1.0/managerserver/key.php
@@ -1,21 +1,20 @@
 <?php
 /*
- * 处理文件备份时，验证是否是非法上传
- * author:张程
- */
+* Token check
+* author: zfan
+*/
 
-//载入初始化文件
 require(dirname(__FILE__) . "/includes/init.inc.php");
 
 $key = $_POST['key'];
 
-//获取备份时的key
+//Get default key
 require(ROOT_PATH . '/admin/cls_config.php');
 $c = new cls_config();
 $c->_construct();
-$deafult = $c->_get("Password");
+$default = $c->_get("Password");
 
-if($key = $deafult)
+if($key == $default)
 {
 	$result = array('result' => true);
 }
